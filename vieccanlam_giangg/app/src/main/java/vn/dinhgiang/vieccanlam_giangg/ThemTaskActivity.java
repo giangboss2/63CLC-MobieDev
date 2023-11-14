83817829cd239a1b3a19cff1df328cf70d00a475
giangboss2 <138667102+giangboss2@users.noreply.github.com>
2023-11-14 10:36:50 +0700

    Initial commit

---
+++ vieccanlam_giangg/app/src/main/java/vn/dinhgiang/vieccanlam_giangg/ThemTaskActivity.java
@@ -0,0 +1,14 @@
+package vn.dinhgiang.vieccanlam_giangg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_them_task);
+    }
+}
